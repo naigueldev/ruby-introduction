82d94598ae20d6f687e2e72f094f49138c690ccc
Naiguel Santos <naiguel.santos@objective.com.br>
2023-09-12 17:53:00 -0300

    first commit

---
+++ part_1/kind_of_data.rb
@@ -0,0 +1,21 @@
+test_integer = 1
+puts "Class: #{test_integer.class}"
+
+test_float = 5.69
+
+puts "Class: #{test_float.class}"
+
+test_boolean = false
+puts "Class: #{test_boolean.class}"
+
+test_boolean = true
+puts "Class: #{test_boolean.class}"
+
+test_string = 'test'
+puts "Class: #{test_string.class}"
+
+test_symbol = :my_symbol
+puts "Class: #{test_symbol.class}"
+
+test_hash = {}
+puts "Class: #{test_hash.class}"
---
+++ part_1/math_operators.rb
@@ -0,0 +1,4 @@
+value_one = 22.00
+value_two = 12.00
+
+puts value_one + value_two
