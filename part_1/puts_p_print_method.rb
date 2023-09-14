cf859acc7efeac0e7451aa0eac1056e45ce0e5bd
ivanahanke <ivana.hanke@objective.com.br>
2023-09-14 15:59:25 -0300

    Add file for print, p and puts method demo

---
+++ part_1/puts_p_print_method.rb
@@ -0,0 +1,25 @@
+1 + 1 #não imprime nada
+############################################################## puts method
+puts 1
+puts 1 + 3
+puts (1 + 3)
+puts "Hello world!"
+
+puts "Feliz dia do \"programador\"!"
+puts 'Feliz dia do "programador"!'
+
+puts
+
+puts 1, 2, 3
+############################################################## print method
+print "Hello world! "
+print "NÃO PULOU A LINHA"
+############################################################## P method
+p 1
+p 1 + 3
+puts (1 + 3)
+p "Hello world!"
+
+puts "Hello world! \n SERÁ QUE PULA A LINHA?"
+p "Hello world! \n SERÁ QUE PULA A LINHA?"
+print "Hello world! \n SERÁ QUE PULA A LINHA?"
