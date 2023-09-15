fb955aae43cd5d98dd8a139d88ff5983bbd3c073
Naiguel Santos <naiguel.santos@objective.com.br>
2023-09-15 12:00:40 -0300

    iterators: map

--- part_1/iterators.rb
+++ part_1/iterators.rb
@@ -6,3 +6,11 @@ end
 
 my_hash = { min: 2, max: 5 }
 my_hash.each { |key, value| puts "Key: #{key}, Value: #{value}" }
+
+puts '>>> Map'
+
+array = %w[a b c]
+puts "array.map(&:upcase): #{array.map(&:upcase)}"
+
+array = [1, 2, 3]
+puts "array.map { |n| n * 2 }: #{array.map { |n| n * 2 }}"
