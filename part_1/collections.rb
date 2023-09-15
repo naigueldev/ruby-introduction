7e94a9ae8d8ffe3784038ed64eb690947b2dd806
Naiguel Santos <naiguel.santos@objective.com.br>
2023-09-15 11:22:26 -0300

    add arrays

---
+++ part_1/collections.rb
@@ -0,0 +1,14 @@
+puts '>>> Arrays'
+
+browsers = ['Chrome', 'Firefox', 'Safari', 'Opera', 'IE']
+
+puts "browsers: #{browsers}"
+puts "browsers.length: #{browsers.length}"
+puts "browsers.count: #{browsers.count}"
+puts "browsers.empty?: #{browsers.empty?}"
+puts "browsers.include?('DuckDuckGo'): #{browsers.include?('DuckDuckGo')}"
+
+browsers.push('DuckDuckGo')
+
+puts "browsers.include?('DuckDuckGo'): #{browsers.include?('DuckDuckGo')}"
+puts "browsers: #{browsers}"
