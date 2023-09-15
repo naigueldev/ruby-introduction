9aea49c4bc336df451d92e927d60dc9bfc7fb8f9
Naiguel Santos <naiguel.santos@objective.com.br>
2023-09-15 08:52:30 -0300

    add math operators

--- part_1/math_operators.rb
+++ part_1/math_operators.rb
@@ -1,4 +1,31 @@
 value_one = 22.00
 value_two = 12.00
 
-puts value_one + value_two
+puts 'Adição:'
+puts "#{value_one} + #{value_two} = #{value_one + value_two}"
+puts
+puts 'Subtração:'
+puts "#{value_one} - #{value_two} = #{value_one - value_two}"
+puts
+puts 'Multiplicação:'
+puts "#{value_one} * #{value_two} = #{value_one * value_two}"
+puts
+puts 'Divisão:'
+puts "#{value_one} / #{value_two} = #{value_one / value_two}"
+puts "#{value_one} / #{value_two} = #{(value_one / value_two).round(2)}"
+value_one = 2.2
+value_two = 0.5
+puts "#{value_one} / #{value_two} = #{value_one / value_two}"
+puts
+puts 'Módulo (resto da divisão):'
+puts "#{value_one} % #{value_two} = #{value_one % value_two}"
+puts
+puts 'Expoente:'
+value_one = 2
+value_two = 4
+puts "#{value_one} ** #{value_two} = #{value_one**value_two}"
+puts
+puts 'Concatenação:'
+value_one = '2'
+value_two = '4'
+puts "#{value_one} + #{value_two} = #{value_one + value_two}"
