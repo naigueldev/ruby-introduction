0957f83392a6e629bc81a9f17b3cff2b071b5b81
ivanahanke <ivana.hanke@objective.com.br>
2023-09-15 09:37:46 -0300

    Add constants and variable demo

---
+++ part_1/constants_variables.rb
@@ -0,0 +1,16 @@
+# puts 'Constantes'
+# CONSTANTE = 1_000
+# puts CONSTANTE
+# CONSTANTE = 2
+# puts CONSTANTE
+
+puts 'Variáveis'
+fruta = 'Manga'
+Fruta = 'Maça'
+FRUTA = 'Banana'
+
+puts fruta
+puts Fruta
+puts FRUTA
+
+#snake_case
